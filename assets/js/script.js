165c679e5374421e29b3970650dfeb2c498f77dd
Maxime <moostafarai@hotmail.fr>
2021-11-23 09:11:37 +0100

    ajout bouton acheter + redimenssionner image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,19 @@
 fetch("assets/json/donnees.json")
-    .then((response) => response.json())
-    .then((data) => {
-        data.results.forEach((element) => {
-            document.getElementById("card").innerHTML += `
-      <div class="m-3 col-lg-3 border border-danger">
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.forEach((element) => {
+      document.getElementById("card").innerHTML += `
+      <div class="m-3 col-lg-3">
         <img src="assets/img/${element.image}" id="picture" class="card-img-top" alt="">
         <div class="card-body">
-            <h5 id="title" class="card-title">${element.title}</h5>
+            <h5 id="title" class="text-center">${element.title}</h5>
             <p id="categorie></p>
-            <p id="description" class="card-text">${element.description}</p>
+            <p id="description" class="">${element.description}</p>
             <a href="#" id="price" class="btn btn-secondary">${element.price}</a>
+            <button>Acheter</button>
+            </div>
             </div>
         </div>
       `;
-        });
     });
+  });
